e6d322928e51239ba20961d2f8242c934a8a3c3a
ChallaBharadwajReddy <challabharadwajreddy@gmail.com>
2025-05-11 10:19:48 +0530

    panda:(Completed v3 , docker image build & needs documenattion )

--- WB-service/app.py
+++ WB-service/app.py
@@ -13,7 +13,7 @@ import redis
 import pickle
 
 # Connect to Redis
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
 
 
 #DB intiation & Migration
